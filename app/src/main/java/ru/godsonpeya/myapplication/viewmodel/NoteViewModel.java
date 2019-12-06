46844e724b61773eba438b575600515f0f77122b
Godson PEYA ESSONGA <peyagodson@ymail.com>
2019-12-06 12:12:50 +0300

    Sample app using architecture component and MVVM

---
+++ app/src/main/java/ru/godsonpeya/myapplication/viewmodel/NoteViewModel.java
@@ -0,0 +1,44 @@
+package ru.godsonpeya.myapplication.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+import ru.godsonpeya.myapplication.database.repositories.NoteRepository;
+import ru.godsonpeya.myapplication.models.Note;
+
+
+public class NoteViewModel extends AndroidViewModel {
+    private NoteRepository repository;
+    private LiveData<List<Note>> allNotes;
+
+    public NoteViewModel(@NonNull Application application) {
+        super(application);
+        repository = new NoteRepository(application);
+        allNotes = repository.getAllNotes();
+    }
+
+    public void insert(Note note) {
+        repository.insert(note);
+    }
+
+    public void update(Note note) {
+        repository.update(note);
+    }
+
+    public void delete(Note note) {
+        repository.delete(note);
+    }
+
+    public void deleteAllNotes() {
+        repository.deleteAllNotes();
+    }
+
+    public LiveData<List<Note>> getAllNotes() {
+        return allNotes;
+    }
+}
